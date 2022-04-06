abf709c92281b5f29255d6a51dda2ef1e2ab5742
abdeljalil.naim@webbdev.nl <Abdelj@lil@1>
2022-04-06 16:37:33 +0200

    feat: test

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ declare const module: any;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	await app.listen(3000);
-	const test = 'test2';
+	const test = 'test3';
 
 	if (module.hot) {
 		module.hot.accept();

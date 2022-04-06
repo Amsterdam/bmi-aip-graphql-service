2b2fd8a46c7e8343cc3a05465d9c5fe7d41e359c
abdeljalil.naim@webbdev.nl <Abdelj@lil@1>
2022-04-06 16:44:21 +0200

    feat: Husky added

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ declare const module: any;
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	await app.listen(3000);
-	const test = 'test4';
+	const test = 'test5';
 
 	if (module.hot) {
 		module.hot.accept();

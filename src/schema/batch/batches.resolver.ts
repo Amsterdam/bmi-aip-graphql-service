4810531412ee88110b1fcd3ddb12ea391706e14e
abdeljalil.naim@webbdev.nl <Abdelj@lil@1>
2022-04-13 13:25:13 +0200

    feat: Create Prisma models that are required for authorization

--- src/schema/batch/batches.resolver.ts
+++ src/schema/batch/batches.resolver.ts
@@ -2,16 +2,16 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 
 import { PrismaService } from '../../prisma.service';
 
-import { BatchModel } from './models/batch.model';
+import { Batch } from './models/batch.model';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-@Resolver((of) => BatchModel)
+@Resolver((of) => Batch)
 export class BatchesResolver {
 	public constructor(private readonly prismaService: PrismaService) {}
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	@Query((returns) => BatchModel, { name: 'batches' })
-	public async GetBatchById(@Args('id') id: string): Promise<BatchModel> {
+	@Query((returns) => Batch, { name: 'batches' })
+	public async GetBatchById(@Args('id') id: string): Promise<Batch> {
 		const batch = this.prismaService.batches.findUnique({
 			where: { id: id },
 		});

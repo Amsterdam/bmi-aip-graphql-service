0712eb9f882d434e546e9fca740302273134a8c6
abdeljalil.naim@webbdev.nl <Abdelj@lil@1>
2022-10-10 14:36:54 +0200

    bugfix: update Asset module

--- src/schema/asset/asset.module.ts
+++ src/schema/asset/asset.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 
 import { PrismaService } from '../../prisma.service';
+import { AuthorizationModule } from '../../authorization/authorization.module';
+import { AuthenticationModule } from '../../authentication/authentication.module';
 
 import { AssetService } from './asset.service';
 import { AssetRepository } from './asset.repository';
+import { CreateAssetHandler } from './commands/create-asset.handler';
+import { UpdateAssetHandler } from './commands/update-asset.handler';
+import { CreateAssetCommand } from './commands/create-asset.command';
+import { UpdateAssetCommand } from './commands/update-asset.command';
+import { AssetResolver } from './asset.resolver';
 
 @Module({
-	providers: [AssetService, AssetRepository, PrismaService],
+	providers: [
+		AssetResolver,
+		AssetService,
+		AssetRepository,
+		PrismaService,
+		CreateAssetCommand,
+		CreateAssetHandler,
+		UpdateAssetCommand,
+		UpdateAssetHandler,
+	],
+	imports: [CqrsModule, AuthorizationModule, AuthenticationModule],
 	exports: [AssetService],
 })
 export class AssetModule {}
--- src/schema/asset/types/asset.repository.interface.ts
+++ src/schema/asset/types/asset.repository.interface.ts
@@ -1,7 +1,9 @@
 import { CreateAssetInput } from '../dto/create-asset.input';
 import { DBAsset } from '../asset.repository';
+import { UpdateAssetInput } from '../dto/update-asset.input';
 
 export interface IAssetRepository {
 	getWritableAssetCodesForCompanyId(companyId: string): Promise<string[]>;
 	createAsset(input: CreateAssetInput): Promise<DBAsset>;
+	updateAsset(input: UpdateAssetInput): Promise<DBAsset>;
 }

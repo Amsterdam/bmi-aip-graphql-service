1bc5dd257af3775dfe936e05f16f538324dbc2e2
Fortunato Geelhoed <f.geelhoed@gmail.com>
2022-12-20 15:12:42 +0100

    feat(nen2767-migration): removed dead bit of code

--- src/command/models/migrate-nen2767-decomposition.model.ts
+++ src/command/models/migrate-nen2767-decomposition.model.ts
@@ -2,23 +2,14 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType({ description: 'migrateNen2767DecompositionCommandResponse' })
 export class MigrateNen2767DecompositionModel {
-	// @Field((type) => Boolean)
-	// done: boolean;
-
 	@Field((type) => [String])
 	errors: string[];
 
 	@Field((type) => [String])
 	log: string[];
 
-	// @Field((type) => [String])
-	// successObjectIds: string[];
-
 	@Field((type) => [String])
 	successSurveyIds: string[];
-
-	// @Field((type) => [String])
-	// failedObjectIds: string[];
 
 	@Field((type) => [String])
 	failedSurveyIds: string[];

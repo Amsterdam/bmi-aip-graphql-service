f9b0a33602611d80a9d12f07c55c125ee90d67dc
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-01-04 16:50:29 +0100

    feat (arkgeography-data): removed fields already in base

--- src/schema/ark-survey/dto/update-ark-survey-geography-data.input.ts
+++ src/schema/ark-survey/dto/update-ark-survey-geography-data.input.ts
@@ -1,6 +1,4 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { Point } from 'graphql-geojson-scalar-types';
-import { Point as PointType } from 'geojson';
 import { IsUUID } from 'class-validator';
 
 import { BaseArkSurveyGeographyDataInput } from './base-ark-survey-geography-data.input';
@@ -10,16 +8,4 @@ export class UpdateArkSurveyGeographyDataInput extends BaseArkSurveyGeographyDat
 	@Field()
 	@IsUUID()
 	public id: string;
-
-	@Field((type) => Point, { nullable: true })
-	ArkGeographyStart?: PointType;
-
-	@Field((type) => Point, { nullable: true })
-	ArkGeographyRDStart?: PointType;
-
-	@Field((type) => Point, { nullable: true })
-	ArkGeographyEnd?: PointType;
-
-	@Field((type) => Point, { nullable: true })
-	ArkGeographyRDEnd?: PointType;
 }

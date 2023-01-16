979116b0525bffbe022e7cfdb7ca02f21b742bc6
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-01-16 17:37:02 +0100

    feat (ark-survey): added validation on the amount of reach segments

--- src/schema/ark-survey/dto/update-ark-survey.input.ts
+++ src/schema/ark-survey/dto/update-ark-survey.input.ts
@@ -1,12 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { GraphQLJSON } from 'graphql-type-json';
+import { Validate } from 'class-validator';
 
 import { JSONValue } from '../../../types/json-value';
+import { ReachSegmentsAmountUnder26 } from '../../../validators/ReachSegmentsAmountUnder26';
 
 import { BaseArkSurveyInput } from './base-ark-survey.input';
 
 @InputType()
 export class UpdateArkSurveyInput extends BaseArkSurveyInput {
 	@Field(() => GraphQLJSON, { nullable: true })
+	@Validate(ReachSegmentsAmountUnder26)
 	public reachSegments?: JSONValue;
 }

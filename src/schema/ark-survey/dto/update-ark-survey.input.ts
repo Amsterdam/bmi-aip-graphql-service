74448b0eb357ae2c5f967000ed6196e255dad81e
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-01-17 10:33:42 +0100

    feat (ark-survey): added custom validators for scores in ReachSegment model

--- src/schema/ark-survey/dto/update-ark-survey.input.ts
+++ src/schema/ark-survey/dto/update-ark-survey.input.ts
@@ -4,6 +4,7 @@ import { Validate } from 'class-validator';
 
 import { JSONValue } from '../../../types/json-value';
 import { ReachSegmentsAmountUnder26 } from '../../../validators/ReachSegmentsAmountUnder26';
+import { NumberWithinRange } from '../../../validators/NumberWithinRange';
 
 import { BaseArkSurveyInput } from './base-ark-survey.input';
 
@@ -11,5 +12,8 @@ import { BaseArkSurveyInput } from './base-ark-survey.input';
 export class UpdateArkSurveyInput extends BaseArkSurveyInput {
 	@Field(() => GraphQLJSON, { nullable: true })
 	@Validate(ReachSegmentsAmountUnder26)
+	@Validate(NumberWithinRange, ['riskScoreDigit', 1.0, 16.0])
+	@Validate(NumberWithinRange, ['failureModeScore', 1.0, 16.0])
+	@Validate(NumberWithinRange, ['consequenceScore', 1.0, 4.0])
 	public reachSegments?: JSONValue;
 }
---
+++ src/validators/NumberWithinRange.ts
@@ -0,0 +1,22 @@
+import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
+
+@ValidatorConstraint({ name: 'numberWithinRange', async: false })
+export class NumberWithinRange implements ValidatorConstraintInterface {
+	validate(body: object[], args: ValidationArguments) {
+		const [property, min, max] = args.constraints;
+		let allValid = true;
+		
+		body.forEach((item) => {
+			if (item[property] < min || item[property] > max) {
+				allValid = false;
+			} 
+		});
+ 
+		console.log({allValid}); 
+		return allValid;
+	}
+
+	defaultMessage(args: ValidationArguments) {
+		return `The number field ${args.constraints[0]} must be between ${args.constraints[1]} and ${args.constraints[2]}`;
+	}
+}

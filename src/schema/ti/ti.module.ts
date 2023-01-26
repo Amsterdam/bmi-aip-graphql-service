d8858995f43700cfb15d418a5bbf1b91ff5ffe4b
Fortunato Geelhoed <f.geelhoed@gmail.com>
2023-01-26 15:43:09 +0100

    fix(models): import paths were not right

--- src/schema/ti/ti.module.ts
+++ src/schema/ti/ti.module.ts
@@ -1,7 +1,6 @@
 import { registerEnumType } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { GetConditionHandler } from 'src/schema/ti/queries/find-conditions.handler';
 
 import { AuthorizationModule } from '../../authorization/authorization.module';
 import { AuthenticationModule } from '../../authentication/authentication.module';
@@ -12,12 +11,13 @@ import { DefectService } from './defect.service';
 import { DefectRepository } from './defect.repository';
 import { GetDefectQuery } from './queries/get-defect.query';
 import { GetDefectHandler } from './queries/get-defect.handler';
-import { GetConditionQuery } from './queries/find-conditions.query';
 import { ConditionResolver } from './condition.resolver';
 import { ConditionService } from './condition.service';
 import { ConditionRepository } from './condition.repository';
 import { CreateConditionHandler } from './commands/create-condition.handler';
 import { UpdateConditionHandler } from './commands/update-condition.handler';
+import { GetConditionQuery } from './queries/get-condition.query';
+import { GetConditionHandler } from './queries/get-condition.handler';
 
 registerEnumType(RepairAdviceCategory, {
 	name: 'RepairAdviceCategory',

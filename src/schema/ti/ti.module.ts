6ea456f0eac52c7bd14544a5f4b8671897393171
abdeljalil.naim@webbdev.nl <Abdelj@lil@1>
2023-01-30 09:58:10 +0100

    feat: ti module updated

--- src/schema/ti/ti.module.ts
+++ src/schema/ti/ti.module.ts
@@ -1,18 +1,18 @@
 import { registerEnumType } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
-import { GetConditionHandler } from 'src/schema/ti/queries/find-conditions.handler';
 
 import { AuthorizationModule } from '../../authorization/authorization.module';
 import { AuthenticationModule } from '../../authentication/authentication.module';
 import { PrismaService } from '../../prisma.service';
+import { GetConditionQuery } from '../ti/queries/get-condition.query';
+import { GetConditionHandler } from '../ti/queries/get-condition.handler';
 
 import { RepairAdviceCategory } from './types';
 import { DefectService } from './defect.service';
 import { DefectRepository } from './defect.repository';
 import { GetDefectQuery } from './queries/get-defect.query';
 import { GetDefectHandler } from './queries/get-defect.handler';
-import { GetConditionQuery } from './queries/find-conditions.query';
 import { ConditionResolver } from './condition.resolver';
 import { ConditionService } from './condition.service';
 import { ConditionRepository } from './condition.repository';

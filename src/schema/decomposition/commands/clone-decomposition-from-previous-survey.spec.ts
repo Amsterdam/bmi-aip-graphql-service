e70f56d5408c05aa2dba8a6c6a7fab4465938fd8
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-01-31 18:06:41 +0100

    feat(clone-decomposition): fixed test for CloneDecompositionFromPreviousSurveyCommand

--- src/schema/decomposition/commands/clone-decomposition-from-previous-survey.spec.ts
+++ src/schema/decomposition/commands/clone-decomposition-from-previous-survey.spec.ts
@@ -4,6 +4,7 @@ import { DecompositionRepository } from '../decomposition.repository';
 import { domainElement } from '../__stubs__';
 
 import { CloneDecompositionFromPreviousSurveyCommand } from './clone-decomposition-from-previous-survey.command';
+import { CloneDecompositionFromPreviousSurveyHandler } from './clone-decomposition-from-previous-survey.handler';
 
 const decompositionRepoMock: MockedObjectDeep<DecompositionRepository> = {
 	findPreviousSurveyId: jest.fn().mockResolvedValue('__PREVIOUS_SURVEY_ID__'),
@@ -14,7 +15,9 @@ const decompositionRepoMock: MockedObjectDeep<DecompositionRepository> = {
 
 describe('CloneDecompositionFromPreviousSurveyCommand', () => {
 	test('executes command', async () => {
-		new CloneDecompositionFromPreviousSurveyCommand('__SURVEY_ID__');
+		const command = new CloneDecompositionFromPreviousSurveyCommand('__SURVEY_ID__');
+		await new CloneDecompositionFromPreviousSurveyHandler(decompositionRepoMock).execute(command);
+
 		expect(decompositionRepoMock.cloneDecomposition).toHaveBeenCalledTimes(1);
 	});
 });

768a0c17e2d67bc86d4325b34a4f401a4856e918
Fortunato <f.geelhoed@gmail.com>
2023-02-02 11:36:00 +0100

    test(measure): added test for defect field resolver (#259)
    * test(measure): added test for defect field resolver
    
    * test(measure): added null test for defect field resolver
    
    * test(measure): fixed test after optimistic refactor

--- src/schema/measure/measure.resolver.spec.ts
+++ src/schema/measure/measure.resolver.spec.ts
@@ -11,17 +11,10 @@ import { CreateMeasureCommand } from './commands/create-measure.command';
 import { Measure } from './models/measure.model';
 import { UpdateMeasureCommand } from './commands/update-measure.command';
 import { DeleteMeasureCommand } from './commands/delete-measure.command';
+import { FindMeasuresQuery } from './queries/find-measures.query';
 
 jest.mock('./measure.service');
 jest.mock('./measure.repository');
-
-const getQueryBusMock = (): MockedObjectDeep<QueryBus> => ({
-	execute: jest.fn((query: any) => {
-		return;
-	}),
-	...(<any>{}),
-});
-const queryBusMock = getQueryBusMock();
 
 const getCommandBusMock = (): MockedObjectDeep<CommandBus> => ({
 	execute: jest.fn((command: any) => {
@@ -31,6 +24,16 @@ const getCommandBusMock = (): MockedObjectDeep<CommandBus> => ({
 				return domainMeasure;
 			case DeleteMeasureCommand.name:
 				return deletedMeasure;
+		}
+	}),
+	...(<any>{}),
+});
+
+const getQueryBusMock = (): MockedObjectDeep<QueryBus> => ({
+	execute: jest.fn((command: any) => {
+		switch (command.constructor.name) {
+			case FindMeasuresQuery.name:
+				return [measure1, measure2];
 		}
 	}),
 	...(<any>{}),
@@ -46,7 +49,7 @@ describe('Decomposition / Measure / Resolver', () => {
 	describe('createMeasure', () => {
 		test('creates and returns an measure', async () => {
 			const commandBusMock = getCommandBusMock();
-			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, queryBusMock);
+			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, getQueryBusMock());
 			const result = await resolver.createMeasure(measureInput);
 			expect(commandBusMock.execute).toHaveBeenCalledTimes(1);
 			expect(commandBusMock.execute).toHaveBeenCalledWith(new CreateMeasureCommand(measureInput));
@@ -59,7 +62,7 @@ describe('Decomposition / Measure / Resolver', () => {
 	describe('updateMeasure', () => {
 		test('updates and returns an measure', async () => {
 			const commandBusMock = getCommandBusMock();
-			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, queryBusMock);
+			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, getQueryBusMock());
 			const result = await resolver.updateMeasure(updateMeasureInput);
 			expect(commandBusMock.execute).toHaveBeenCalledTimes(1);
 			expect(commandBusMock.execute).toHaveBeenCalledWith(new UpdateMeasureCommand(updateMeasureInput));
@@ -72,7 +75,7 @@ describe('Decomposition / Measure / Resolver', () => {
 	describe('deleteMeasure', () => {
 		test('soft-deletes and returns an measure', async () => {
 			const commandBusMock = getCommandBusMock();
-			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, queryBusMock);
+			const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, getQueryBusMock());
 			const result = await resolver.deleteMeasure(domainMeasure.id);
 			expect(commandBusMock.execute).toHaveBeenCalledTimes(1);
 			expect(commandBusMock.execute).toHaveBeenCalledWith(new DeleteMeasureCommand(domainMeasure.id));
@@ -83,10 +86,25 @@ describe('Decomposition / Measure / Resolver', () => {
 		});
 	});
 
-	test('getSurveyMeasures returns an array of measure objects', async () => {
-		const commandBusMock = getCommandBusMock();
-		const resolver = new MeasureResolver(new MeasureService(measureRepo), commandBusMock, queryBusMock);
-		const measures = await resolver.getSurveyMeasures('ad18b7c4-b2ef-4e6e-9bbf-c33360584cd7');
-		expect(measures).toEqual([measure1, measure2]);
+	describe('getSurveyMeasures', () => {
+		test('returns an array of measure objects', async () => {
+			const queryBusMock = getQueryBusMock();
+			const resolver = new MeasureResolver(new MeasureService(measureRepo), getCommandBusMock(), queryBusMock);
+			const measures = await resolver.getSurveyMeasures(measure1.surveyId);
+			expect(queryBusMock.execute).toHaveBeenCalledTimes(1);
+			expect(queryBusMock.execute).toHaveBeenCalledWith(new FindMeasuresQuery(measure1.surveyId));
+
+			expect(measures).toEqual([measure1, measure2]);
+		});
+
+		test('optionally returns defect properties with a field resolver', async () => {
+			const queryBusMock = getQueryBusMock();
+			const resolver = new MeasureResolver(new MeasureService(measureRepo), getCommandBusMock(), queryBusMock);
+			const [measureWithDefect, measureWithoutDefect] = await resolver.getSurveyMeasures(
+				'ad18b7c4-b2ef-4e6e-9bbf-c33360584cd7',
+			);
+			expect(measureWithDefect.defect).toEqual(measure1.defect);
+			expect(measureWithoutDefect.defect).toEqual(null);
+		});
 	});
 });

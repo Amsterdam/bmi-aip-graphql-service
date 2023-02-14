7c7898f4b726235637e410bc10f23d8dd371a9dc
Fortunato Geelhoed <f.geelhoed@gmail.com>
2023-02-14 09:14:57 +0100

    test(measure): fixed faulty test

--- src/schema/measure/queries/calculate-measure-cost-with-surcharge.spec.ts
+++ src/schema/measure/queries/calculate-measure-cost-with-surcharge.spec.ts
@@ -1,12 +1,12 @@
 import { measure } from '../__stubs__';
 
-import { CalculateMeasureCostHandler } from './calculate-measure-cost.handler';
 import { CalculateMeasureCostWithSurchargeQuery } from './calculate-measure-cost-with-surcharge.query';
+import { CalculateMeasureCostWithSurchargeHandler } from './calculate-measure-cost-with-surcharge.handler';
 
 describe('CalculateMeasureCostWithSurchargeQuery', () => {
 	test('executes query', async () => {
 		const query = new CalculateMeasureCostWithSurchargeQuery(measure);
-		const result = await new CalculateMeasureCostHandler().execute(query);
+		const result = await new CalculateMeasureCostWithSurchargeHandler().execute(query);
 		expect(result.costWithSurcharge).toEqual(4962.54);
 	});
 });

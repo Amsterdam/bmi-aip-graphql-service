1b1ae869b8ab6b1ad8d5c83837d4f771629561e5
Muchu Yu <m.yu@amsterdam.nl>
2023-03-07 13:32:31 +0100

    fix: merge create and update to one save input

--- src/schema/facade-follow-up-survey/dto/update-facade-follow-up-survey.input.ts
+++
@@ -1,6 +0,0 @@
-import { InputType } from '@nestjs/graphql';
-
-import { BaseFacadeFollowUpSurveyInput } from './base-facade-follow-up-survey.input';
-
-@InputType()
-export class UpdateFacadeFollowUpSurveyInput extends BaseFacadeFollowUpSurveyInput {}

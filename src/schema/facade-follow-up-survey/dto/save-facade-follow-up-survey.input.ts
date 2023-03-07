721cfea2b7274898205fa8f4fd67ab6e6d8bef6b
Muchu Yu <m.yu@amsterdam.nl>
2023-03-07 13:43:47 +0100

    feature: get data from inspection standard data filed from survey table

--- src/schema/facade-follow-up-survey/dto/save-facade-follow-up-survey.input.ts
+++ src/schema/facade-follow-up-survey/dto/save-facade-follow-up-survey.input.ts
@@ -1,9 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional } from 'class-validator';
 
 import { BaseFacadeFollowUpSurveyInput } from './base-facade-follow-up-survey.input';
 
 @InputType()
 export class SaveFacadeFollowUpSurveyInput extends BaseFacadeFollowUpSurveyInput {
-	@Field((type) => String)
-	public remarks: string;
+	@IsOptional()
+	@Field({ nullable: true })
+	public remarks?: string;
 }

cd3a1da462f88bcd1e781e68250e31f2f0a97fbe
Muchu Yu <m.yu@amsterdam.nl>
2023-03-07 13:31:23 +0100

    fix: merge create and update to one save command and handler

--- src/schema/facade-follow-up-survey/commands/update-facade-follow-up-survey.command.ts
+++
@@ -1,5 +0,0 @@
-import { UpdateFacadeFollowUpSurveyInput } from '../dto/update-facade-follow-up-survey.input';
-
-export class UpdateFacadeFollowUpSurveyCommand {
-	public constructor(public readonly data: UpdateFacadeFollowUpSurveyInput) {}
-}

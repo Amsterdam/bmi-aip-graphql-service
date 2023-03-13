3ec9dc7d6513635e76f4b40742c0ed34ff380808
Muchu Yu <m.yu@amsterdam.nl>
2023-03-13 16:30:46 +0100

    fix: remove unnecessary command handler and input

--- src/schema/facade-follow-up-survey/commands/save-facade-follow-up-survey.command.ts
+++
@@ -1,5 +0,0 @@
-import { SaveFacadeFollowUpSurveyInput } from '../dto/save-facade-follow-up-survey.input';
-
-export class SaveFacadeFollowUpSurveyCommand {
-	public constructor(public readonly data: SaveFacadeFollowUpSurveyInput) {}
-}

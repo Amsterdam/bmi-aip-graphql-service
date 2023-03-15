c644b6e81e907ffba6a48fc1b2e356068e243f36
Muchu Yu <m.yu@amsterdam.nl>
2023-03-15 21:56:35 +0100

    fix: remove unused import

--- src/schema/facade-follow-up-survey/dto/update-facade-follow-up-survey.input.ts
+++ src/schema/facade-follow-up-survey/dto/update-facade-follow-up-survey.input.ts
@@ -1,5 +1,4 @@
 import { InputType } from '@nestjs/graphql';
-// import { IsOptional } from 'class-validator';
 
 import { BaseFacadeFollowUpSurveyInput } from './base-facade-follow-up-survey.input';
 

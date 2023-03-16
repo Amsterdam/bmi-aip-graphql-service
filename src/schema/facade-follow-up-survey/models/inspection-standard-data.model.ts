e2605e0d06a01e5a303103ff9b23966cc0f2d6fb
Muchu Yu <m.yu@amsterdam.nl>
2023-03-16 15:17:45 +0100

    fix: remove unused code and rename

--- src/schema/facade-follow-up-survey/models/inspection-standard-data.model.ts
+++
@@ -1,7 +0,0 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-
-@ObjectType({ description: 'inspectionStandardData' })
-export class InspectionStandardData {
-	@Field((type) => String, { nullable: true })
-	remarks?: string;
-}

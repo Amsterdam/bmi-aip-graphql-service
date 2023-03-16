d69d7273146cb5cc7d925ed419e4a70da47892af
Abdeljalil Naim <a.naim@amsterdam.nl>
2023-03-16 15:37:28 +0100

    feat: translate failureMode metaData props (#310)
    * feat: translate failureMode metaData props
    
    * chore: PR comments verwerkt
    
    ---------
    
    Co-authored-by: abdeljalil.naim@webbdev.nl <Abdelj@lil@1>

--- src/schema/failure-mode/models/failure-mode-meta-data.model.spec.ts
+++ src/schema/failure-mode/models/failure-mode-meta-data.model.spec.ts
@@ -3,19 +3,19 @@ import { FailureModeMetaData } from './failure-mode-meta-data.model';
 describe('FailureMode / Model / FailureModeMetaData', () => {
 	test('constructs a FailureModeMetaData instance', () => {
 		const failureModeMetaData = new FailureModeMetaData();
-		failureModeMetaData.faalOorzaak = null;
-		failureModeMetaData.bronVanFalen = null;
-		failureModeMetaData.gevolgVanFalen = null;
-		failureModeMetaData.faaloorzaakAnders = null;
-		failureModeMetaData.bronVanFalenAnders = null;
+		failureModeMetaData.failureCause = null;
+		failureModeMetaData.sourceOfFailure = null;
+		failureModeMetaData.consequenceOfFailure = null;
+		failureModeMetaData.causeOfFailureOther = null;
+		failureModeMetaData.sourceOfFailureOther = null;
 
 		expect(failureModeMetaData).toBeInstanceOf(FailureModeMetaData);
 		expect(failureModeMetaData).toEqual({
-			faalOorzaak: null,
-			bronVanFalen: null,
-			gevolgVanFalen: null,
-			faaloorzaakAnders: null,
-			bronVanFalenAnders: null,
+			failureCause: null,
+			sourceOfFailure: null,
+			consequenceOfFailure: null,
+			causeOfFailureOther: null,
+			sourceOfFailureOther: null,
 		});
 	});
 });

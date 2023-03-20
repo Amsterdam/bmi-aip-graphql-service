b6fd4853e63fa772da8a7160e35b2045cae548b5
Muchu Yu <m.yu@amsterdam.nl>
2023-03-20 10:19:57 +0100

    feature: update saveArkSurvey and inputs

--- src/schema/ark-survey/dto/update-ark-survey.input.ts
+++ src/schema/ark-survey/dto/update-ark-survey.input.ts
@@ -1,6 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { ValidateNested } from 'class-validator';
+import { IsDate, IsOptional, MaxLength, ValidateNested } from 'class-validator';
 
+import { ArkInspectionStandardDataInput } from './ark-inspection-standard-data.input';
 import { BaseArkSurveyInput } from './base-ark-survey.input';
 import { ReachSegmentInput } from './reach-segment.input';
 
@@ -9,4 +10,28 @@ export class UpdateArkSurveyInput extends BaseArkSurveyInput {
 	@Field(() => [ReachSegmentInput], { nullable: true })
 	@ValidateNested({ each: true })
 	public reachSegments?: ReachSegmentInput[];
+
+	@IsOptional()
+	@MaxLength(255)
+	@Field((type) => String, { nullable: true })
+	public preparedAuthor?: string;
+
+	@IsOptional()
+	@IsDate()
+	@Field((type) => Date, { nullable: true })
+	public preparedDate?: Date;
+
+	@IsOptional()
+	@MaxLength(255)
+	@Field((type) => String, { nullable: true })
+	public verifiedAuthor?: string;
+
+	@IsOptional()
+	@IsDate()
+	@Field((type) => Date, { nullable: true })
+	public verifiedDate?: Date;
+
+	@IsOptional()
+	@Field((type) => ArkInspectionStandardDataInput, { nullable: true })
+	public inspectionStandardData?: ArkInspectionStandardDataInput;
 }

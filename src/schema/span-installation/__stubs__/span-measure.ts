9e7e98c9dceedad98edf8adddbcb60a3bf918253
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-04-26 10:17:15 +0200

    refactor: one line export const declaration

--- src/schema/span-installation/__stubs__/span-measure.ts
+++ src/schema/span-installation/__stubs__/span-measure.ts
@@ -4,14 +4,17 @@ import { SpanMeasureFactory } from '../span-measure.factory';
 import { CreateSpanMeasureInput } from '../dto/create-span-measure.input';
 import { UpdateSpanMeasureInput } from '../dto/update-span-measure-input';
 
-const spanMeasure1 = new SpanMeasure();
-spanMeasure1.id = '9812a0c4-9cb4-4df2-b490-7a5648922f79';
-spanMeasure1.optionId = '9812a0c4-9cb4-4df2-b490-7a5648922f79';
-spanMeasure1.decompositionId = 'ad18b7c4-b2ef-4e6e-9bbf-c33360584cd7';
-spanMeasure1.decompositionType = 'SpanMeasure';
-spanMeasure1.description = '__NAME__';
-
-export { spanMeasure1 };
+export const spanMeasure1: SpanMeasure = {
+	id: '9812a0c4-9cb4-4df2-b490-7a5648922f79',
+	surveyId: '9812a0c4-9cb4-4df2-b490-7a5648922f79',
+	optionId: '9812a0c4-9cb4-4df2-b490-7a5648922f79',
+	decompositionId: 'ad18b7c4-b2ef-4e6e-9bbf-c33360584cd7',
+	decompositionType: 'SpanMeasure',
+	description: '__NAME__',
+	measureItems: [],
+	createdAt: undefined,
+	updatedAt: undefined,
+};
 
 export const spanMeasureRaw: Omit<DomainSpanMeasure, 'id'> = {
 	surveyId: '0deb07f3-28f5-47e1-b72a-d1b2a19d4670',

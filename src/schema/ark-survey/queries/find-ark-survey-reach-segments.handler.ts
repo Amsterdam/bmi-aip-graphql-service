aee6737be28e770f4c25e45f6df8d35b38f9eebc
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-06-05 16:40:49 +0200

    chore: removed console.logs

--- src/schema/ark-survey/queries/find-ark-survey-reach-segments.handler.ts
+++ src/schema/ark-survey/queries/find-ark-survey-reach-segments.handler.ts
@@ -9,7 +9,6 @@ export class FindArkSurveyReachSegmentsHandler implements IQueryHandler<FindArkS
 	constructor(private service: ReachSegmentService) {}
 
 	async execute(query: FindArkSurveyReachSegmentsQuery): Promise<ReachSegment[]> {
-		console.log('find!');
 		return this.service.findReachSegments(query.arkSurveyId);
 	}
 }

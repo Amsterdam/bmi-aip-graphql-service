90742e5a50cc154aa794408e0cbfa2b7d50ad855
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-06-08 16:17:59 +0200

    feat: finished `HasDecompositionItemGotDamageHandler` for all entities + fixed test

--- src/schema/span-installation-survey/queries/has-decomposition-item-got-damage.handler.ts
+++ src/schema/span-installation-survey/queries/has-decomposition-item-got-damage.handler.ts
@@ -3,6 +3,10 @@ import { QueryHandler, IQueryHandler } from '@nestjs/cqrs';
 import { JunctionBoxSurveyService } from '../junction-box-survey.service';
 import { SpanDecompositionType } from '../../span-installation/types/span-decomposition-type';
 import { FacadeSurveyService } from '../facade-survey.service';
+import { MastSurveyService } from '../mast-survey.service';
+import { NodeSurveyService } from '../node-survey.service';
+import { TensionWireSurveyService } from '../tension-wire-survey.service';
+import { LuminaireSurveyService } from '../luminaire-survey.service';
 
 import { HasDecompositionItemGotDamageQuery } from './has-decomposition-item-got-damage.query';
 
@@ -11,6 +15,10 @@ export class HasDecompositionItemGotDamageHandler implements IQueryHandler<HasDe
 	constructor(
 		private junctionBoxService: JunctionBoxSurveyService,
 		private facadeSurveyService: FacadeSurveyService,
+		private mastSurveyService: MastSurveyService,
+		private nodeSurveyService: NodeSurveyService,
+		private tensionWireSurveyService: TensionWireSurveyService,
+		private luminaireSurveyService: LuminaireSurveyService,
 	) {}
 
 	async execute(query: HasDecompositionItemGotDamageQuery): Promise<boolean> {
@@ -18,8 +26,20 @@ export class HasDecompositionItemGotDamageHandler implements IQueryHandler<HasDe
 			case SpanDecompositionType.spanJunctionBox:
 				return this.junctionBoxService.hasDamage(query.decompositionId);
 				break;
-			case SpanDecompositionType.spanJunctionBox:
+			case SpanDecompositionType.spanSupportSystemFacade:
 				return this.facadeSurveyService.hasDamage(query.decompositionId);
+				break;
+			case SpanDecompositionType.spanSupportSystemMast:
+				return this.mastSurveyService.hasDamage(query.decompositionId);
+				break;
+			case SpanDecompositionType.spanSupportSystemNode:
+				return this.nodeSurveyService.hasDamage(query.decompositionId);
+				break;
+			case SpanDecompositionType.spanSupportSystemTensionWire:
+				return this.tensionWireSurveyService.hasDamage(query.decompositionId);
+				break;
+			case SpanDecompositionType.spanLuminaire:
+				return this.luminaireSurveyService.hasDamage(query.decompositionId);
 				break;
 		}
 
--- src/schema/span-installation-survey/queries/has-decomposition-item-got-damage.spec.ts
+++ src/schema/span-installation-survey/queries/has-decomposition-item-got-damage.spec.ts
@@ -2,11 +2,36 @@ import { MockedObjectDeep } from 'ts-jest';
 
 import { JunctionBoxSurveyService } from '../junction-box-survey.service';
 import { SpanDecompositionType } from '../../span-installation/types/span-decomposition-type';
+import { FacadeSurveyService } from '../facade-survey.service';
+import { LuminaireSurveyService } from '../luminaire-survey.service';
+import { MastSurveyService } from '../mast-survey.service';
+import { NodeSurveyService } from '../node-survey.service';
+import { TensionWireSurveyService } from '../tension-wire-survey.service';
 
-import { HasDecompositionItemGotDamageQuery } from './has-decomposition-item-got-damage.query';
 import { HasDecompositionItemGotDamageHandler } from './has-decomposition-item-got-damage.handler';
+import { HasDecompositionItemGotDamageQuery } from './has-decomposition-item-got-damage.query';
 
 const junctionBoxServiceMock: MockedObjectDeep<JunctionBoxSurveyService> = {
+	hasDamage: jest.fn().mockResolvedValue(true),
+	...(<any>{}),
+};
+const facadeSurveyServiceMock: MockedObjectDeep<FacadeSurveyService> = {
+	hasDamage: jest.fn().mockResolvedValue(true),
+	...(<any>{}),
+};
+const mastSurveyServiceMock: MockedObjectDeep<MastSurveyService> = {
+	hasDamage: jest.fn().mockResolvedValue(true),
+	...(<any>{}),
+};
+const nodeSurveyServiceMock: MockedObjectDeep<NodeSurveyService> = {
+	hasDamage: jest.fn().mockResolvedValue(true),
+	...(<any>{}),
+};
+const tensionWireSurveyServiceMock: MockedObjectDeep<TensionWireSurveyService> = {
+	hasDamage: jest.fn().mockResolvedValue(true),
+	...(<any>{}),
+};
+const luminaireSurveyServiceMock: MockedObjectDeep<LuminaireSurveyService> = {
 	hasDamage: jest.fn().mockResolvedValue(true),
 	...(<any>{}),
 };
@@ -16,7 +41,14 @@ const decompositionId = '3cc978ca-3b4e-476a-b44c-d4cf6f6ac8f7';
 describe('HasDecompositionItemGotDamageHandler', () => {
 	test('executes query', async () => {
 		const command = new HasDecompositionItemGotDamageQuery(decompositionId, SpanDecompositionType.spanJunctionBox);
-		await new HasDecompositionItemGotDamageHandler(junctionBoxServiceMock).execute(command);
+		await new HasDecompositionItemGotDamageHandler(
+			junctionBoxServiceMock,
+			facadeSurveyServiceMock,
+			mastSurveyServiceMock,
+			nodeSurveyServiceMock,
+			tensionWireSurveyServiceMock,
+			luminaireSurveyServiceMock,
+		).execute(command);
 
 		expect(junctionBoxServiceMock.hasDamage).toHaveBeenCalledTimes(1);
 		expect(junctionBoxServiceMock.hasDamage).toHaveBeenCalledWith(decompositionId);

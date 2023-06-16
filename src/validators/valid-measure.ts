e7488dbdc89c8e92d2280ebc48d953a809ebcef4
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-06-16 16:07:01 +0200

    fix: removed need for 'spanMeasureItemOptions' in normalized-data.measure.json + fixed importer match spanMeasureItemOptions on referenceNumber rather than id as that field is not persistent across multiple generation attempts

--- src/validators/valid-measure.ts
+++ src/validators/valid-measure.ts
@@ -1,29 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
 
-import {
-	spanMeasureItems as availableSpanMeasureItemOptions,
-	spanMeasureOptions as availableSpanMeasureOptions,
-} from '../schema/span-installation/types/measure-options';
+import { spanMeasureOptions as availableSpanMeasureOptions } from '../schema/span-installation/types/measure-options';
 
 @ValidatorConstraint({ name: 'optionId' })
 @Injectable()
 export class MeasureOptionValidation implements ValidatorConstraintInterface {
 	async validate(value: string, args: ValidationArguments): Promise<boolean> {
-		let dataSource;
-		switch (args.targetName) {
-			case 'CreateSpanMeasureInput':
-				dataSource = availableSpanMeasureOptions;
-				break;
-			case 'SpanMeasureItemInput':
-				dataSource = availableSpanMeasureItemOptions;
-				break;
-			default:
-				throw new Error('Decorator applied to unspecified target.');
-				break;
-		}
-
-		dataSource = availableSpanMeasureOptions;
+		const dataSource = availableSpanMeasureOptions;
 
 		switch (args.targetName) {
 			case 'CreateSpanMeasureInput':

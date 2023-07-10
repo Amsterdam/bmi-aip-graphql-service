4832a263f9a77ee52d11e5e67d7b38684e9dc3ff
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-07-10 16:54:56 +0200

    fix: made `quantityActual` nullable again

---
+++ src/schema/span-installation/dto/span-measure-items-used-quantities-input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional, IsUUID } from 'class-validator';
+@InputType()
+export class SpanMeasureItemUsedQuantitiesInput {
+	@Field()
+	@IsUUID()
+	public id: string;
+
+	@Field((type) => Number, { nullable: true })
+	@IsOptional()
+	quantityActual: number;
+
+	@Field((type) => Boolean, { nullable: true })
+	@IsOptional()
+	active: boolean;
+}

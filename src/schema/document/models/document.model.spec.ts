a2cfffb6e959767acd1a8303ae5df01461b2954b
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-07-20 11:40:33 +0200

    chore: clean up test

--- src/schema/document/models/document.model.spec.ts
+++ src/schema/document/models/document.model.spec.ts
@@ -1,21 +1,24 @@
 import { Document } from './document.model';
 
+let document;
+
 describe('Document', () => {
+	beforeAll(async () => {
+		document = new Document();
+		document.assetCode = '123456';
+		document.fileName = 'test.pdf';
+	});
+
 	it('should create a new document instance', () => {
-		const document = new Document();
 		expect(document).toBeDefined();
 	});
 
 	it('should have an assetCode property of type string', () => {
-		const document = new Document();
-		document.assetCode = '123456';
 		expect(document.assetCode).toBeDefined();
 		expect(typeof document.assetCode).toBe('string');
 	});
 
 	it('should have a fileName property of type string', () => {
-		const document = new Document();
-		document.fileName = 'test.pdf';
 		expect(document.fileName).toBeDefined();
 		expect(typeof document.fileName).toBe('string');
 	});

e6953e17de4920a570dfb26b3669d4f3aecd1f10
Fortunato Geelhoed <f.geelhoed@gmail.com>
2023-07-25 09:18:15 +0200

    chore(ts): resolved selected typescript errors

--- src/schema/span-installation-survey/queries/get-decomposition-item-damage.spec.ts
+++ src/schema/span-installation-survey/queries/get-decomposition-item-damage.spec.ts
@@ -3,14 +3,24 @@ import { MockedObjectDeep } from 'ts-jest';
 import { junctionBoxSurvey } from '../__stubs__';
 import { JunctionBoxSurveyService } from '../junction-box-survey.service';
 import { SpanDecompositionItemType } from '../../span-installation/types/span-decomposition-item-type';
+import { FacadeSurveyService } from '../facade-survey.service';
+import { MastSurveyService } from '../mast-survey.service';
+import { NodeSurveyService } from '../node-survey.service';
+import { TensionWireSurveyService } from '../tension-wire-survey.service';
+import { LuminaireSurveyService } from '../luminaire-survey.service';
 
-import { GetDecompositionItemDamageQuery } from './get-decomposition-item-damage.query';
 import { GetDecompositionItemDamageHandler } from './get-decomposition-item-damage.handler';
+import { GetDecompositionItemDamageQuery } from './get-decomposition-item-damage.query';
 
 const junctionBoxServiceMock: MockedObjectDeep<JunctionBoxSurveyService> = {
 	getJunctionBoxSurveyOnPermanentId: jest.fn().mockResolvedValue(junctionBoxSurvey),
 	...(<any>{}),
 };
+const facadeSurveyServiceMock: MockedObjectDeep<FacadeSurveyService> = { ...(<any>{}) };
+const mastSurveyServiceMock: MockedObjectDeep<MastSurveyService> = { ...(<any>{}) };
+const nodeSurveyServiceMock: MockedObjectDeep<NodeSurveyService> = { ...(<any>{}) };
+const tensionWireSurveyServiceMock: MockedObjectDeep<TensionWireSurveyService> = { ...(<any>{}) };
+const luminaireSurveyServiceMock: MockedObjectDeep<LuminaireSurveyService> = { ...(<any>{}) };
 
 const decompositionItemId = '3cc978ca-3b4e-476a-b44c-d4cf6f6ac8f7';
 
@@ -20,7 +30,14 @@ describe('GetDecompositionItemDamageHandler', () => {
 			decompositionItemId,
 			SpanDecompositionItemType.spanJunctionBox,
 		);
-		await new GetDecompositionItemDamageHandler(junctionBoxServiceMock).execute(command);
+		await new GetDecompositionItemDamageHandler(
+			junctionBoxServiceMock,
+			facadeSurveyServiceMock,
+			mastSurveyServiceMock,
+			nodeSurveyServiceMock,
+			tensionWireSurveyServiceMock,
+			luminaireSurveyServiceMock,
+		).execute(command);
 
 		expect(junctionBoxServiceMock.getJunctionBoxSurveyOnPermanentId).toHaveBeenCalledTimes(1);
 		expect(junctionBoxServiceMock.getJunctionBoxSurveyOnPermanentId).toHaveBeenCalledWith(decompositionItemId);

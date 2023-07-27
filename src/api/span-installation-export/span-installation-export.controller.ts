0e3e8d08eb93a3548052b21439051d5ecc23a8a9
bennyvdhoogen <70586325+bennyvdhoogen@users.noreply.github.com>
2023-07-27 18:36:53 +0200

    feat: added @Roles decorator to newly added routes

--- src/api/span-installation-export/span-installation-export.controller.ts
+++ src/api/span-installation-export/span-installation-export.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { Response } from 'express';
+import { RoleMatchingMode, Roles } from 'nest-keycloak-connect';
 
 import { ExportBatchDataQuery } from './queries/export-batch-data.query';
 import { ExportAllDataQuery } from './queries/export-all-data.query';
@@ -10,6 +11,7 @@ export class SpanInstallationExportController {
 	constructor(private readonly queryBus: QueryBus) {}
 
 	@Get('export-by-batch/:batchId')
+	@Roles({ roles: ['realm:aip_owner', 'realm:aip_admin', 'realm:aip_survey'], mode: RoleMatchingMode.ANY })
 	async exportBatchToXLSX(@Param('batchId') batchId: string, @Res() res: Response) {
 		const { xlsxBuffer, fileName } = await this.queryBus.execute(new ExportBatchDataQuery(batchId));
 		res.set({
@@ -20,6 +22,7 @@ export class SpanInstallationExportController {
 	}
 
 	@Get('export-all')
+	@Roles({ roles: ['realm:aip_owner', 'realm:aip_admin', 'realm:aip_survey'], mode: RoleMatchingMode.ANY })
 	async exportToXLSX(@Res() res: Response) {
 		const { xlsxBuffer, fileName } = await this.queryBus.execute(new ExportAllDataQuery());
 		res.set({
